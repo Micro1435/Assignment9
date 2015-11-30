6a4e05282be123cdaa82e24b94e120711dbcead6
Michael <michael.montella@colorado.edu>
2015-11-30 16:02:24 -0700

    Initial Commit

---
+++ Assignment9/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment9
+//
+//  Created by Michael Montella on 11/30/15.
+//  Copyright © 2015 Michael Montella. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
